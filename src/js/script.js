60a6737ac3aed92df5d6ccb855db1ea1fb4f98cc
kittypitty <samutic40@gmail.com>
2022-11-14 22:07:46 +0300

    add validateForm functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,17 @@
-console.log('Hello')
+console.log("webpack")
+
+function isNumber(num) {
+    return typeof num === 'number' && !isNaN(num);
+}
 
 
+
+function validateForm () {
+    let x = document.forms["phoneForm"]["q"].value;
+    if (x.length <= 6 || isNumber(x)) {
+        alert("Incorrect data. Please, try one more time");
+        document.forms["phoneForm"]["search"].value = ""
+        return false;
+    }
+}
+ window.validateForm = validateForm;
